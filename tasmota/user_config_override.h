adb3f159269d8236259c0a74921bf569625636c7
fjshshj <fjshshj1@gmail.com>
2025-06-29 00:51:26 +0700

    Update user_config_override.h

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -9,9 +9,6 @@
 #define WIFI_CONFIG_TOOL WIFI_RETRY // thử lại Wi-Fi
 #define WIFI_WAIT_TIME   120        // 2 phút trước khi vào AP mode
 
-// Tắt BLE để tránh lỗi NimBLEDevice.h
-#undef USE_ESP32_BLE
-
 // Bật các thành phần cơ bản bạn cần
 #define USE_IR_REMOTE
 #define USE_LD2410
